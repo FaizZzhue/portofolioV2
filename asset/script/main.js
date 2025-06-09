6b2722a11d2eec8549e8cfbcd3664f9570682c9a
unknown <achmadfaizyudharamadhan_2428240113@mhs.mdp.ac.id>
2025-06-09 13:34:53 +0700

    Last Updateding

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,5 +1,6 @@
 console.log('Hello Faiz')
 
+// Humburger menu
 function toggleMenu() {
     const nav = document.getElementById('navMenu');
     nav.classList.toggle('active');
@@ -16,6 +17,48 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+// Tema menu
+document.querySelector('.button-thema').addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    const icon = document.querySelector('.button-thema i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('bxs-moon');
+        icon.classList.add('bxs-sun');
+    } else {
+        icon.classList.remove('bxs-sun');
+        icon.classList.add('bxs-moon');
+    }
+});
+
+// Contact menu
+const phoneIcon = document.getElementById('phoneIcon');
+document.addEventListener('mousemove', (e) => {
+    const rect = phoneIcon.getBoundingClientRect();
+    const iconX = rect.left + rect.width / 2;
+    const iconY = rect.top + rect.height / 2;
+
+    const distance = Math.hypot(e.clientX - iconX, e.clientY - iconY);
+
+    if (distance < 100) { // radius 100px
+        phoneIcon.style.transform = 'rotate(180deg)';
+    } else {
+        phoneIcon.style.transform = 'rotate(0deg)';
+    }
+});
+
+// Skills menu
+document.addEventListener("DOMContentLoaded", () => {
+    const flip = document.querySelectorAll('.skills-cirular')
+
+    flip.forEach(card => {
+        card.addEventListener('click', () => {
+            card.classList.toggle('flipped');
+        });
+    });
+});
+
+// Email menu
 document.querySelector(".button-contact").addEventListener("click", () => {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
@@ -26,4 +69,17 @@ document.querySelector(".button-contact").addEventListener("click", () => {
     } else {
         alert("Please fill out all fields.");
     }
+});
+
+// Send menu
+const btn = document.querySelector('.button-contact');
+const icon = btn.querySelector('i');
+
+btn.addEventListener('click', () => {
+    icon.classList.add('fly');
+
+    // reset animasi 
+    icon.addEventListener('animationend', () => {
+        icon.classList.remove('fly');
+    }, { once: true });
 });
